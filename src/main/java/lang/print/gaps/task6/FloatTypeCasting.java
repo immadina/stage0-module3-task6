bc3405926c17c9aaf0446888b4d2b88776e4aa9b
madina <91madina@gmail.com>
2022-08-17 15:00:29 +0600

    Module #3, Task #6

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int roundedNumber = (int) (numberToBeRounded + 0.5);
+
+        System.out.println(roundedNumber);
     }
 }
